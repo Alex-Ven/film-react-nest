ecabcfd6d8f3811d894d39f4bf1acaffda934b67
Alex_V <venedykhin@yandex.ru>
2025-06-30 12:24:46 +1200

    Дороботка для deploy nginx.prod.conf CORS заголовки

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,10 @@ async function bootstrap() {
   const logger = loggerFactory.createLogger(loggerType);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // или массив ['http://localhost:5173']
+    origin: [
+      'https://afisha.justforstudy.nomorepartiessbs.ru',
+      'http://localhost:5173' // для разработки
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
